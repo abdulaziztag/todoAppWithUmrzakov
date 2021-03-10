691f646932b5b35e89e7bec9909c178efe9d7436
thisisumurzakov <thisisumurzakov@gmail.com>
2021-03-10 22:21:29 +0500

    post

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -4,4 +4,5 @@ from .views import TodoListView, TodoDetailView
 urlpatterns = [
     path('get_all/', TodoListView.as_view()),
     path('<int:pk>/', TodoDetailView.as_view()),
+    path('post/', TodoDetailView.as_view()),
 ]
